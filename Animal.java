02c1e486d1165bdfe6c58742d946ca4eec6845f5
wmathieu <wmathieu@udel.edu>
2018-09-17 20:10:38 -0400

    Animal classes added, and eclipse integration

---
+++ Animal.java
@@ -0,0 +1,18 @@
+
+public abstract class Animal {
+	String name;
+	int legs;
+	
+	public Animal(String name, int legs) {
+		this.name = name;
+		this.legs = legs;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getLegs() {
+		return legs;
+	}
+}
---
+++ Dog.java
@@ -0,0 +1,6 @@
+
+public class Dog extends Animal {
+	public Dog(String name, int legs) {
+		super(name, legs);
+	}
+}
